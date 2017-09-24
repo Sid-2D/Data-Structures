ed6ebe14fb795d88847929fe5ae428ac7858c4a6
Sid <sid6327@gmail.com>
2017-09-24 23:43:17 +0530

    Adding new logic with driver program.

--- HeterogenousList.c
+++ HeterogenousList.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node n;
+
 struct element1 {
 	int data;
 	void *next;
@@ -10,6 +12,19 @@ struct element2 {
 	char data;
 	void *next;
 };
+
+struct node {
+	void *data;
+	int type;
+	n *next;
+};
+
+n* init() {
+	n *tmp = (n*)malloc(sizeof(n));
+	tmp -> data = NULL;
+	tmp -> type = 0;
+	tmp -> next = NULL;
+}
 
 struct element1* init1(int num) {
 	struct element1 *tmp = (struct element1*)malloc(sizeof(struct element1));
@@ -29,5 +44,22 @@ int main() {
 	struct element1 *root = init1(1);
 	struct element2 *node = init2('v');
 	root -> next = node;
+	n *genRoot = init();
+	n *nextNode = init();
+	int data1 = 10;
+	char data2 = 'x';
+	genRoot -> data = &data1;
+	nextNode -> data = &data2;
+	nextNode -> type = 1;
+	genRoot -> next = nextNode;
+	n *temp = genRoot;
+	while (temp != NULL) {
+		if (temp -> type == 0) {
+			printf("%d\n", *(int *)temp -> data);
+		} else {
+			printf("%c\n", *(char *)temp -> data);
+		}
+		temp = temp -> next;
+	}
 	return 0;
 }
